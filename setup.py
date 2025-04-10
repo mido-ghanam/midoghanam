1dc8eef8d5358e3732e7149ba5ae01bbead7b3d7
Mohammed Ghanam <127614412+mido-ghanam@users.noreply.github.com>
2025-04-10 03:29:38 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import Require, setup, find_packages
 
 setup(
   name='midoghanam',
-  version='0.0.2',
+  version='0.0.3',
   author='Mohamed Ahmed Ghanam',
   author_email='mghanam883@outlook.com',
   description='مكتبتي الشخصية – midoghanam',
