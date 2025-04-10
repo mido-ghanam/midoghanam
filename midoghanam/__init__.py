35fcdbf033204f9d399f4e1740a520079082c7e3
Mohammed Ghanam <127614412+mido-ghanam@users.noreply.github.com>
2025-04-10 02:10:57 +0200

    Add files via upload

--- midoghanam/__init__.py
+++ midoghanam/__init__.py
@@ -1,3 +1,24 @@
+import requests, json
+
+def get_user_key(api_url, register_api_url, json_dic):
+  if not all([api_url, register_api_url, json_dic]):
+    print("Please provide the API URL.")
+    return None
+  while True:
+    r = requests.get(api_url)
+    if r.status_code == 201 and r.json().get("status") == "True":
+      return r.json().get("user")
+    else:
+      try:
+        r.json()
+      except:
+        return "Server is down right now!"
+      try:
+        r = requests.post(register_api_url, json=json_dic)
+        if r.status_code == 201 and r.json().get("status") == "True":
+          continue
+      except:
+        pass
 
 def get_preferred_sentence(preferred_language, ar_sentence, en_sentence):
   if not all([preferred_language, ar_sentence, en_sentence]):
